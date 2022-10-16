3dfee4a5bf0d78cc3d6cc4eb90c565347394ccc8
Pranav Singh <pranav08singh@gmail.com>
2022-10-16 19:30:34 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (50.72%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> ans;
+        
+        for(int i=0; i<s.size(); i++){
+            
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[' ){
+                ans.push(s[i]);
+            }
+            
+            else{
+                
+                if(!ans.empty()){
+                    char top = ans.top();
+                    
+                    if((s[i]==')' && top=='(') || (s[i]=='}' && top=='{') || (s[i]==']' && top=='[')){
+                        ans.pop();
+                    }else{
+                        return false;
+                    }
+                }else{
+                    return false;
+                }
+                
+            }
+        }
+        
+        
+        if(ans.empty()){
+            return true;
+        }return false;
+    }
+};
