4b84653756e8efa34de40ded393150ab92d6d503
Pranav Singh <pranav08singh@gmail.com>
2023-04-09 22:07:53 +0530

    Update 0017-letter-combinations-of-a-phone-number.py

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -5,6 +5,7 @@ class Solution(object):
     def letterCombinations(self, digits):
         mapping = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', 
                    '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+        
         if len(digits) == 0:
             return []
         if len(digits) == 1:
